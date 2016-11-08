82218ac2197aeab59c3d4654fb6f4d1d99a7adac
Dariusz Szuksztul <dszuksztul@atlassian.com>
2016-11-08 11:52:05 +0100

    VPP-2159 - powershell script that can download files from MediaAPI

--- backup.ps1
+++ backup.ps1
@@ -4,22 +4,48 @@ $password    = '******'
 $destination = 'C:\Backups'
 $attachments = $false
 
+$hostname    = "$account.atlassian.net"
 $today       = Get-Date -format yyyyMMdd
 $credential  = New-Object System.Management.Automation.PSCredential($username, (ConvertTo-SecureString $password -AsPlainText -Force))
 
+if ($PSVersionTable.PSVersion.Major -lt 4) {
+    throw "Script requires at least PowerShell version 4. Get it here: https://www.microsoft.com/en-us/download/details.aspx?id=40855"
+}
+
 # Login
-Invoke-WebRequest -Method Post -Uri "https://$account.atlassian.net/login" -SessionVariable session -Body @{username = $username; password = $password} | Out-Null
+Invoke-WebRequest -Method Post -Uri "https://$hostname/login" -SessionVariable session -Body @{username = $username; password = $password} | Out-Null
 
 # Request backup
-Invoke-RestMethod -Method Post -Uri "https://$account.atlassian.net/rest/obm/1.0/runbackup" -WebSession $session -ContentType 'application/json' -Body (@{cbAttachments = $attachments} | ConvertTo-Json -Compress) | Out-Null
+Invoke-RestMethod -Method Post -Uri "https://$hostname/rest/obm/1.0/runbackup" -WebSession $session -ContentType 'application/json' -Body (@{cbAttachments = $attachments} | ConvertTo-Json -Compress) | Out-Null
 
 # Wait for backup to finish
 do {
-    $status = Invoke-RestMethod -Method Get -Uri "https://$account.atlassian.net/rest/obm/1.0/getprogress" -WebSession $session
-    $status.alternativePercentage -match "(\d+)"
-    Write-Progress -Activity 'Creating backup' -Status $status.alternativePercentage -PercentComplete $Matches[1]
+    $status = Invoke-RestMethod -Method Get -Headers @{"Accept"="application/json"} -Uri "https://$hostname/rest/obm/1.0/getprogress" -WebSession $session
+    if ($status.alternativePercentage -match "(\d+)") {
+        $percentage = $Matches[1]
+        if ([int]$percentage -gt 100) {
+            $percentage = "100"
+        }
+        Write-Progress -Activity 'Creating backup' -Status $status.alternativePercentage -PercentComplete $percentage
+    }
     Start-Sleep -Seconds 5
 } while($status.alternativePercentage -ne 'Estimated progress: 100 %')
 
 # Download
-Invoke-WebRequest "https://$account.atlassian.net/webdav/backupmanager/JIRA-backup-$today.zip" -Credential $credential -OutFile (Join-Path -Path $destination -ChildPath "JIRA-backup-$today.zip")
+if ([bool]($status.PSObject.Properties.Name -match "failedMessage")) {
+    throw $status.failedMessage
+}
+
+$pathName = $status.fileName
+if ($pathName -match "ondemandbackupmanager/download/.+/(.*)") {
+    $fileName = $Matches[1]
+    Write-Host "Downloading: $fileName to JIRA-backup-$today.zip"
+    $download_headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $download_headers.Add("Accept", '*/*')
+    $progressPreference = 'Continue'
+    Invoke-WebRequest -Method Get -Headers @{"Accept"="*/*"} -WebSession $session -Uri "https://$hostname/$pathName" -OutFile (Join-Path -Path $destination -ChildPath "JIRA-backup-$today.zip")
+} else {
+    Write-Host "Dowlnoading: $pathName to JIRA-backup-$today.zip"
+    $progressPreference = 'Continue'
+    Invoke-WebRequest "https://$hostname/webdav/backupmanager/$pathName" -Credential $credential -OutFile (Join-Path -Path $destination -ChildPath "JIRA-backup-$today.zip")    
+}
