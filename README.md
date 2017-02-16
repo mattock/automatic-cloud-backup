40fddeddc2783aee3a96666649bb00e321db0bdb
Samuli Seppänen <samuli@openvpn.net>
2017-02-16 15:20:45 +0000

    Cleanups and support for backing up Confluence
    - Add Confluence backup support
    - Store cookie in user's home directory
    - Cache and reuse authorization cookie
    - Remove some bashisms
    - Proper indentation to improve code readability
    - Add documentation
    - Remove support for old webdav URLs
    - Allow running the script several times in a row
    - Exit with an error message if authorization has failed
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- README.md
+++ README.md
@@ -1,3 +1,58 @@
-This is a backup script taken for automatic Cloud backups, taken from [How to Automate Backups for JIRA Cloud applications](https://confluence.atlassian.com/display/JIRAKB/How+to+Automate+Backups+for+JIRA+Cloud+applications).
+# Introduction
 
-It is currently community maintained.
+This script can be used to automate backups of Atlassian Cloud JIRA and 
+Confluence instances. It is based on Atlassian Labs' automatic-cloud-backup 
+script:
+
+* https://bitbucket.org/atlassianlabs/automatic-cloud-backup
+
+This repository contains several fixes to the original backup.sh script, which 
+is not even functional in it current state.
+
+This fork was created because Atlassian Labs had ignored all community pull 
+requests and issues for several months.
+
+# Configuration
+
+You will need to create a configuration file for the script to work. To do this, 
+copy backup.sh.vars.example to $HOME/.backup.sh.vars and edit it to match your 
+environment.
+
+# Usage
+
+Using the script is straightforward. To backup JIRA:
+
+    ./backup.sh jira
+
+To backup Confluence (wiki):
+
+    ./backup.sh wiki
+
+# Implementation
+
+The script generates an authorization cookie using your Atlassian credentials if
+
+* a cookie does not exist, or
+* a cookie exists but is over 24 hours old
+
+Atlassian Cloud only allows _creation_ of one backup per day, counted separately 
+for JIRA and Confluence. However, reuse of the cookie allows running the backup 
+script several times in the row; if a new backup could not be created, then the 
+previously created backup is fetched.
+
+The authorization cookie is used for three things:
+
+* Triggering the creation of a new backup at the Atlassian Cloud end
+* Checking backup creation progress
+* Downloading the backup once it's ready
+
+# Contributing
+
+If you find a problem please file an issue, or better yet, a pull request.
+
+# License
+
+The license of the original script is unclear. All contributions by me (Samuli 
+Seppänen) are licensed under the 2-Clause BSD License:
+
+* https://opensource.org/licenses/BSD-2-Clause
--- backup.sh
+++ backup.sh
@@ -2,12 +2,32 @@
 
 CONFIG="$HOME/.backup.sh.vars"
 
-if [[ -r "$CONFIG" ]]; then
+if [ -r "$CONFIG" ]; then
     . $CONFIG
 else
     echo "Usable to load $CONFIG! Please create one based on backup.sh.vars.example"
     exit 1
 fi
+
+usage() {
+    echo "Usage: $0 jira|wiki" 
+    echo
+    exit 1
+}
+
+if [ "$1" = "jira" ]; then
+    SUBDIR=""
+    DOWNLOAD_URL="https://${INSTANCE}"
+elif [ "$1" = "wiki" ]; then
+    SUBDIR="/wiki"
+    DOWNLOAD_URL="https://${INSTANCE}${SUBDIR}/download"
+else
+    usage
+fi
+
+BASENAME=$1
+RUNBACKUP_URL="https://${INSTANCE}${SUBDIR}/rest/obm/1.0/runbackup"
+PROGRESS_URL="https://${INSTANCE}${SUBDIR}/rest/obm/1.0/getprogress.json"   
 
 # Grabs cookies and generates the backup on the UI. 
 TODAY=$(TZ=$TIMEZONE date +%Y%m%d)
@@ -15,57 +35,51 @@ TODAY=$(TZ=$TIMEZONE date +%Y%m%d)
 #Check if we should overwrite the previous backup or append a timestamp to 
 #prevent just that. The former is useful when an external backup program handles 
 #backup rotation.
-if [[ $TIMESTAMP == "true" ]]; then
-    OUTFILE="${LOCATION}/JIRA-backup-${TODAY}.zip"
-elif [[ $TIMESTAMP == "false" ]]; then
-    OUTFILE="${LOCATION}/JIRA-backup.zip"
+if [ $TIMESTAMP = "true" ]; then
+    OUTFILE="${LOCATION}/$BASENAME-backup-${TODAY}.zip"
+elif [ $TIMESTAMP = "false" ]; then
+    OUTFILE="${LOCATION}/$BASENAME-backup.zip"
 else
     echo "ERROR: invalid value for TIMESTAMP: should be either \"true\" or \"false\""
     exit 1
 fi
 
-COOKIE_FILE_LOCATION=jiracookie
-curl --silent --cookie-jar $COOKIE_FILE_LOCATION -X POST "https://${INSTANCE}/rest/auth/1/session" -d "{\"username\": \"$USERNAME\", \"password\": \"$PASSWORD\"}" -H 'Content-Type: application/json' --output /dev/null
-#The $BKPMSG variable will print the error message, you can use it if you're planning on sending an email
-BKPMSG=$(curl -s --cookie $COOKIE_FILE_LOCATION --header "X-Atlassian-Token: no-check" -H "X-Requested-With: XMLHttpRequest" -H "Content-Type: application/json"  -X POST https://${INSTANCE}/rest/obm/1.0/runbackup -d '{"cbAttachments":"true" }' )
+COOKIE_FILE_LOCATION="$HOME/.backup.sh-cookie"
+
+# Only generate a new cookie if one does not exist, or if it is more than 24 
+# hours old. This is to allow reuse of the same cookie until a new backup can be 
+# triggered.
+find $COOKIE_FILE_LOCATION -mtime -1 2> /dev/null |grep $COOKIE_FILE_LOCATION 2>&1 > /dev/null
+if [ $? -ne 0 ]; then
+    curl --silent --cookie-jar $COOKIE_FILE_LOCATION -X POST "https://${INSTANCE}/rest/auth/1/session" -d "{\"username\": \"$USERNAME\", \"password\": \"$PASSWORD\"}" -H 'Content-Type: application/json' --output /dev/null
+fi
+
+# The $BKPMSG variable will print the error message, you can use it if you're planning on sending an email
+BKPMSG=$(curl -s --cookie $COOKIE_FILE_LOCATION --header "X-Atlassian-Token: no-check" -H "X-Requested-With: XMLHttpRequest" -H "Content-Type: application/json"  -X POST $RUNBACKUP_URL -d '{"cbAttachments":"true" }' )
  
-#Checks if the backup procedure has failed
-if [ "$(echo "$BKPMSG" | grep -ic backup)" -ne 0 ]; then
-rm $COOKIE_FILE_LOCATION
-exit
+# Checks if we were authorized to create a new backup
+if [ "$(echo "$BKPMSG" | grep -c Unauthorized)" -ne 0 ]; then
+    echo "ERROR: authorization failure"
+    exit
 fi
 
 #Checks if the backup exists every 10 seconds, 20 times. If you have a bigger instance with a larger backup file you'll probably want to increase that.
-for (( c=1; c<=20; c++ ))
-do
-PROGRESS_JSON=$(curl -s --cookie $COOKIE_FILE_LOCATION https://${INSTANCE}/rest/obm/1.0/getprogress.json)
-FILE_NAME=$(echo "$PROGRESS_JSON" | sed -n 's/.*"fileName"[ ]*:[ ]*"\([^"]*\).*/\1/p')
+for (( c=1; c<=20; c++ )) do
+    PROGRESS_JSON=$(curl -s --cookie $COOKIE_FILE_LOCATION $PROGRESS_URL)
+    FILE_NAME=$(echo "$PROGRESS_JSON" | sed -n 's/.*"fileName"[ ]*:[ ]*"\([^"]*\).*/\1/p')
 
-if [[ $PROGRESS_JSON == *"error"* ]]; then
-break
-fi
+    echo $PROGRESS_JSON|grep error > /dev/null && break
 
-if [ ! -z "$FILE_NAME" ]; then
-break
-fi
-sleep 10
+    if [ ! -z "$FILE_NAME" ]; then
+        break
+    fi
+    sleep 10
 done
 
-#If after 20 attempts it still fails it ends the script.
-if [ -z "$FILE_NAME" ];
-then
-rm $COOKIE_FILE_LOCATION
-exit
+# If after 20 attempts it still fails it ends the script.
+if [ -z "$FILE_NAME" ]; then
+    exit
 else
-
-#If it's confirmed that the backup exists the file get's copied to the $LOCATION directory.
-if [[ $FILE_NAME == *"ondemandbackupmanager/download"* ]]; then
-#Download the new way, starting Nov 2016
-curl -s -L --cookie $COOKIE_FILE_LOCATION "https://${INSTANCE}/$FILE_NAME" -o "$OUTFILE" 
-else
-#Backward compatible download that will not be supported after Nov 2016
-wget --user=$USERNAME --password=$PASSWORD -t 0 --retry-connrefused "https://${INSTANCE}/webdav/backupmanager/JIRA-backup-${TODAY}.zip" -O "$OUTFILE" >/dev/null 2>/dev/null
+    # Download the new way, starting Nov 2016
+    curl -s -L --cookie $COOKIE_FILE_LOCATION "$DOWNLOAD_URL/$FILE_NAME" -o "$OUTFILE"
 fi
-
-fi
-rm $COOKIE_FILE_LOCATION
